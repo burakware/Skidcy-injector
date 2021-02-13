942efa2d4014c07cfeafe81610040038332819af
burak1080p <63106031+burakimsibisey@users.noreply.github.com>
2021-02-13 16:23:19 +0300

    Update Main.java
    everyone bro

--- src/main/java/spicy/Main.java
+++ src/main/java/spicy/Main.java
@@ -6,5 +6,22 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(final String[] args) {}
+    //spicy: removed this method but skidcy;;;;;;;;;;;;;;
+    public static void main(final String[] args) {
+        final StringBuilder sb = new StringBuilder();
+        try {
+            final Scanner sc = new Scanner(new File(String.valueOf(System.getProperty("user.home")) + "/Desktop/Spicy", "sopid.txt"));
+            while (sc.hasNextLine()) {
+                sb.append(sc.nextLine());
+            }
+            System.out.println(sb.toString());
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        final File file = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        Util.attach(sb.toString(), file);
+        }
+    }
 }
